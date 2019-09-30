63eded8c0c3799eb2b9ed941b82623d54b1c22da
aflahbrilli <aflahbrilli891@gmail.com>
2019-09-30 15:38:24 +0700

    Bundle dan Method

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,14 +7,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import id.ac.polinema.idealbodyweight.Fragment.AboutFragment;
+import id.ac.polinema.idealbodyweight.Fragment.BrocaIndexFragment;
+import id.ac.polinema.idealbodyweight.Fragment.MenuFragment;
 
+public class MainActivity extends AppCompatActivity implements
+		MenuFragment.OnFragmentInteractionListener,
+		BrocaIndexFragment.OnFragmentInteractionListener {
+
+
+	private MenuFragment menuFragment;
+	private AboutFragment aboutFragment;
+	private BrocaIndexFragment brocaIndexFragment;
 	// Deklarasikan atribut Fragment di sini
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		aboutFragment = AboutFragment.newInstance("Aflah Brilli");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		menuFragment = new MenuFragment();
+		brocaIndexFragment = new BrocaIndexFragment();
+
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, menuFragment)
+				.commit();
 	}
 
 	@Override
@@ -25,8 +42,31 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-		// TODO: Tambahkan penanganan menu di sini
 
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onBrocaIndexButtonClicked() {
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, brocaIndexFragment)
+				.commit();
+
+	}
+
+	@Override
+	public void onBodyMassIndexButtonClicked() {
+
+	}
+
+	@Override
+	public void onCalculateBrocaIndexClicked(float index) {
+
 	}
 }
